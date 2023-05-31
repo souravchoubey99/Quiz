b0d16ddafb896238196db4758d0848811a09b4a4
souravchoubey99 <132815884+souravchoubey99@users.noreply.github.com>
2023-05-31 08:01:41 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,85 @@
+print("Welcome to Our Quiz Competition")
+while True:
+    Question1 = "A. Who is the First Prime Minister of India?"
+    print(Question1)
+    print("1. H. D. Deve Gowda")
+    print("2. Pt. Jawaharlal Nehru")
+    print("3. Manmohan Singh")
+    print("4. Narendra  Damodar Das Modi")
+    print("5. Indira Gandhi")
+    x=int(input("Answer:"))
+    if x== 2:
+        a="correct"
+        print(a)
+    else:
+        break
+    Question2 = "B. Who is the First Vice Prime Minister of India?"
+    print(Question2)
+    print("1. Sardar Vallabhbhai Patel ")
+    print("2. Pt. Jawaharlal Nehru")
+    print("3. Manmohan Singh")
+    print("4. Narendra  Damodar Das Modi")
+    print("5. Indira Gandhi")
+    x=int(input("Answer:"))
+    if(x==1):
+        a="correct"
+        print(a)
+    else:
+        break
+    Question3 = "C. What is national bird of India?"
+    print(Question3)
+    print("1. Crow")
+    print("2. Owl")
+    print("3. Peacock")
+    print("4. Duck ")
+    print("5. Eagle ")
+    x = int(input("Answer:"))
+    if x == 3:
+        a = "correct"
+        print(a)
+    else:
+        break
+    Question4 = "D. What is national animal of India?"
+    print(Question4)
+    print("1. Lion ")
+    print("2. Tiger")
+    print("3. Horse")
+    print("4. Elephant")
+    print("5. Cow")
+    x = int(input("Answer:"))
+    if (x == 2):
+        a = "correct"
+        print(a)
+    else:
+        break
+    Question5 = "E. What is national game  of India?"
+    print(Question5)
+    print("1. Hockey")
+    print("2. Cricket")
+    print("3. Carrom")
+    print("4. Tennis")
+    print("5. Chess")
+    x = int(input("Answer:"))
+    if x == 1:
+        a = "correct"
+        print(a)
+    else:
+        break
+
+    Question6 = "F. What is national River of India?"
+    print(Question6)
+    print("1. Ganga ")
+    print("2. Yamuna")
+    print("3. Indus")
+    print("4. Baharamputra")
+    print("5. Hindon")
+    x = int(input("Answer:"))
+    if (x == 1):
+        a = "correct"
+        print(a)
+    else:
+        break
+    break
+
+
+
